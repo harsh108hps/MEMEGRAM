45a7e59d48825a6f985dd00fead2d321a2b3e020
Harsh Pratap Singh <harshpratapsingh39900@gmail.com>
2025-05-16 01:33:29 +0530

    my commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
+import ContentBody from "./components/ContentBody";
 import Footer from "./components/Footer";
+
 import Navbar from "./components/Navbar";
+
 
 
 function App() {
   return (
     <>
       <Navbar/>
- 
+     <ContentBody/>
       <Footer/>
     </>
   );
