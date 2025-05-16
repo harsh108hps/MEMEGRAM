16a14a55cb24a8582c3d5bafc23489785d7f7d74
RabinarayanS <rabinarayansahoo9348@gamil.com>
2025-05-16 16:35:18 +0530

    feed filter

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
@@ -23,6 +25,7 @@ function App() {
         <Route path="*" element={<NotFound />} />
       </Routes>
       <Footer />
+      <ToastContainer position="top-right" autoClose={3000} />
     </>
   );
 }
